93784d00d17994076fc470bb717f635b54c52ce3
nicxd531 <101049406+nicxd531@users.noreply.github.com>
2023-01-05 19:29:25 +0100

    Update MainInput.jsx
    hey

--- src/contact/MainInput.jsx
+++ src/contact/MainInput.jsx
@@ -7,7 +7,7 @@ const MainInput = () => {
     return (
         <div className="main-input">
           <h3>message me</h3>
-          <form name="contact" method="POST" netlify netlify-honeypot="bot-field" hidden>
+          <form name="contact" method="POST" netlify netlify-honeypot="bot-field" >
             <input 
             required
             name='name'

70a4a3b09c76268f73146660d478882ec924e149
Ola Olasunkanmi <finixd531@gmail.com>
2024-02-06 14:49:36 +0100

    addded

--- src/contact/MainInput.jsx
+++ src/contact/MainInput.jsx
@@ -7,7 +7,7 @@ const MainInput = () => {
     return (
         <div className="main-input">
           <h3>message me</h3>
-          <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true" netlify>
+          <form name="contact"   netlify>
             <input 
             required
             name='name'

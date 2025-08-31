d8568d920cd785f42f72526f9f1ac1724035fbfd
Ola Olasunkanmi <finixd531@gmail.com>
2025-08-31 10:50:46 +0100

    image change

--- src/about/Aboutimg.jsx
+++ src/about/Aboutimg.jsx
@@ -1,16 +1,16 @@
 const AboutImg = () => {
     // main about image component
-    return ( 
-        <div 
-        data-aos="fade-up"
-                data-aos-anchor-placement="center-bottom"
-        className="about-img">
+    return (
+        <div
+            data-aos="fade-up"
+            data-aos-anchor-placement="center-bottom"
+            className="about-img">
             <div></div>
             <div>
-                <img src="/image/user.jpg" alt="profiile picture"/>
+                <img src="/image/user.user.jpeg" alt="profiile picture" />
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default AboutImg;
